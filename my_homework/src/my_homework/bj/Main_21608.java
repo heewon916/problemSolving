f9672b485c66578097f6a37068cdb4ba2b5e7651
kimhiwon <kimhiwon@naver.com>
2025-08-01 09:29:57 +0900

    create: 0801 java Project

---
+++ my_homework/src/my_homework/bj/Main_21608.java
@@ -0,0 +1,13 @@
+package my_homework.bj;
+import java.util.*; 
+import java.io.*; 
+public class Main_21608 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null; 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
