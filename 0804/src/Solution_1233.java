231052be5c5ecc6fc0e06d1f2738beaf9b80880d
kimhiwon <kimhiwon@naver.com>
2025-08-04 22:20:37 +0900

    add: 0804 swea1233 PS done.

--- 0804/src/Solution_1233.java
+++ 0804/src/Solution_1233.java
@@ -4,20 +4,42 @@ import java.io.*;
 public class Solution_1233 {
 
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		StringBuilder sb = new StringBuilder();
+		System.setIn(new FileInputStream("C:/SSAFY/homework/0804/Test1233.txt"));
 		StringTokenizer st = null; 
+		StringBuilder sb = new StringBuilder(); 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 완전이진트리가 주어진다. 노드의 개수는 최대 200개 
-		// 
-		for(int tc=1; tc<10; tc++) {
+		// 숫자 8이 4번 정점에 위치 -> 4 8 
+		// 연산자 /이 2번 정점에 위치 -> 2 / 4 5 (왼쪽, 오른쪽 자식) 
+		// 연산이 불가능한 경우 -> 리프 노드에 연산자가 있을 때/ 피연산자 2개의 부모 노드가 피연산자일 때
+		int T = 10; 
+		for(int tc=1; tc<=T; tc++) {
+			int answer = 1; 
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
-			
-			
-			
-			sb.append("#").append(tc).append(" ").append("\n");
+			for(int i=0; i<N; i++) {
+
+				st = new StringTokenizer(br.readLine(), " ");
+				int tokenCount = st.countTokens();
+				
+				if(tokenCount == 4) {
+					// not leaf node ->  피연산자 불가능 
+					st.nextToken();
+					String op = st.nextToken();
+					st.nextToken();
+					st.nextToken();
+					if(op.matches("-?\\d+")) answer = 0; 
+				}else if(tokenCount == 2){
+					st.nextToken();
+					String value = st.nextToken();
+					if(!value.matches("-?\\d+")) answer = 0; 
+				} else {
+					answer = 0; 
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
 		}
+		System.out.println(sb.toString());
+		
 	}
 
 }
