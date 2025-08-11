f4f7f17a126c34a98a4fd7292c4f30b4496ca154
kimhiwon <kimhiwon@naver.com>
2025-08-11 14:01:41 +0900

    add: 0811 homework

---
+++ 0811/src/Main_bj_3109_빵집_서울_8반_김희원.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main_bj_3109_빵집_서울_8반_김희원 {
+
+	public static void main(String[] args) throws Exception {
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		
+	}
+
+}
