54f772b7b9e97bcad4b68dc06b7b9d1f68d8fdf5
kimhiwon <kimhiwon@naver.com>
2025-08-12 17:40:29 +0900

    add: 0811 bj 3109

--- 0811/src/Main_bj_3109_빵집_서울_8반_김희원.java
+++ 0811/src/Main_bj_3109_빵집_서울_8반_김희원.java
@@ -2,9 +2,13 @@ import java.util.*;
 import java.io.*; 
 
 public class Main_bj_3109_빵집_서울_8반_김희원 {
+	/*
+	 * 포인트: dfs에서 리턴 타입을 boolean으로 둔다. 이를 통해서 성공/ 실패의 경우를 나눌 수 있다.
+	 * DFS/ 백트래킹 문제에서 매우 중요한 기법 -> 기억해두기 
+	 */
 	static int answer = 0; // 놓을 수 있는 파이프라인의 최대개수 
 	static int R, C; 
-	static String[][] mat; 
+	static char[][] mat; 
 	static int[] di = {-1, 0, 1};
 	public static void main(String[] args) throws Exception {
 		StringTokenizer st = null;
@@ -14,45 +18,36 @@ public class Main_bj_3109_빵집_서울_8반_김희원 {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		mat = new String[R][C]; 
+		mat = new char[R][C]; 
 		for(int i=0; i<R; i++) {
-			String[] t = br.readLine().split("");  
-			for(int j=0; j<C; j++) {
-				mat[i][j] = t[j];  
-			}
+			mat[i] = br.readLine().toCharArray(); /*포인트*/
 		}
  
 		// 0열의 각 위치에서 갈 수 있는 곳 가고, 마지막까지 도달하면 전부 x 처리 필요 
 		// dfs로 진행됨 
 		for(int i=0; i<R; i++) {
-			if(mat[i][0].equals(".")) {
-//				System.out.println("dfs called: " + i + ", " + 0);
-				dfs(i, 0);
+			if(dfs(i, 0)) {
+				answer++;
 			}
 		}
 		sb.append(answer);
 		System.out.println(sb.toString());
 	}
-	static boolean able = true; 
-	static void dfs(int i, int j) {
+	static boolean dfs(int i, int j) {
+		mat[i][j] = 'x'; 
 		if(j == C-1) {
-//			System.out.println("proceed at " + i + " " + j);
-			answer++; 
-			return; 
+			return true; 
 		}
 		for(int d=0; d<3; d++) {
 			int ni = i + di[d];
 			int nj = j + 1;
-			if(0<=ni && ni<R && 0<=nj && nj<C) {
-				if(mat[ni][nj].equals(".")) {
-//					System.out.println("goto" + ni + " " + nj);
-					mat[ni][nj] = "x"; 
-					dfs(ni, nj); 
-					return; 
+			if(0<=ni && ni<R) {
+				if(mat[ni][nj] == '.') {
+					if(dfs(ni, nj))	return true; /*포인트: 결국 dfs가 true가 될 때 함수가 완전히 종료됨.*/
 				}
 			}
-//			able = false; 
 		}
+		return false; 
 	}
 
 }
