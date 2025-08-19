b154dd8c6360f784f4e4039384709b90e247bef0
kimhiwon <kimhiwon@naver.com>
2025-08-19 15:10:12 +0900

    add: 0819 swea 1873 HW

---
+++ my_homework/TestA_직전연습/Main_14567.java
@@ -0,0 +1,11 @@
+import java.io.*; 
+import java.util.*; 
+
+public class Main_14567 {
+  public static void main(String[] args){
+    StringBuilder sb = new StringBuilder(); 
+    BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
+
+    
+  }
+}
--- my_homework/src/letsGetA/Main_0815_bj_17136_색종이붙이기.java
+++ my_homework/src/letsGetA/Main_0815_bj_17136_색종이붙이기.java
@@ -15,11 +15,11 @@ public class Main_0815_bj_17136_색종이붙이기 {
 				mat[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for(int i=0; i<10; i++) {
-			for(int j=0; j<10; j++) {
-				if(mat[i][j] == 1)
-			}
-		}
+		// for(int i=0; i<10; i++) {
+		// 	for(int j=0; j<10; j++) {
+		// 		if(mat[i][j] == 1)
+		// 	}
+		// }
 		
 	}
 	static final int[] dx = {0, 1, 1}; 
