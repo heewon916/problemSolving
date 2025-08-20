2c630878b2ec951caa7a3e3b4eab488be09d61ee
kimhiwon <kimhiwon@naver.com>
2025-08-20 16:00:38 +0900

    add: 0820 HW swea 7793 done

--- 0820/src/Solution_d5_7793_오나의여신님_서울_8반_김희원.java
+++ 0820/src/Solution_d5_7793_오나의여신님_서울_8반_김희원.java
@@ -21,74 +21,81 @@ public class Solution_d5_7793_오나의여신님_서울_8반_김희원 {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken()); // NxM 
 			mat = new char[N][M]; 
+			visited = new boolean[N][M]; 
+			devTime = new int[N][M];
+			ArrayDeque<int[]> devQ = new ArrayDeque<>(); 
+			ArrayDeque<int[]> sQ = new ArrayDeque<>(); 
 			
-			int[] start = new int[2]; 
+			int sx = -1, sy = -1; 
 			for(int i=0; i<N; i++) {
 				String s = br.readLine(); 
 				for(int j=0; j<M; j++) {
 					mat[i][j] = s.charAt(j);
-					if(mat[i][j] == 'S') start = new int[] {i, j};
+					devTime[i][j] = Integer.MAX_VALUE; 
+					if(mat[i][j] == 'S') {
+						sx = i; sy = j; 
+					} else if (mat[i][j] == '*') {
+						devTime[i][j] = 0; 
+						devQ.offer(new int[] {i, j});
+					}
 				}
 			}
 			
-			boolean able = true; 
-			int time = 0; 
-			v = new boolean[N][M]; 
+			// 악마가 확산될 때, 각 좌표는 어느 time에 확산되는지 미리 계산한다. 
+			while(!devQ.isEmpty()) {
+				int[] cur = devQ.poll(); 
+				for(int d=0; d<4; d++) {
+					int nx = cur[0] + dx[d];
+					int ny = cur[1] + dy[d]; 
+					if(0<=nx && nx<N && 0<=ny && ny<M) {
+						if(mat[nx][ny] == '.' && devTime[nx][ny] == Integer.MAX_VALUE) {
+							devTime[nx][ny] = devTime[cur[0]][cur[1]] + 1; 
+							devQ.offer(new int[] {nx, ny});
+						}
+					}
+				}
+			}
 			
-			// 시작점을 큐에 넣고, bfs 할 준비 
-			q = new ArrayDeque<>(); 
-			q.add(new int[] {start[0], start[1]});
-			v[start[0]][start[1]] = true; 
+			// 위에서 구한 악마가 확산되는 time에 맞게
+			// S는 현재 갈 수 있는 곳 중에서 time 시점에 *가 아닌 곳으로 가면 된다. 
 			
-			while(!q.isEmpty()) {
-				time++; 
-				// 1초마다 악마의 손아귀 상하좌우로 퍼져나감 
-				for(int i=0; i<N; i++) {
-					for(int j=0; j<M; j++) {
-						if(mat[i][j] == '*') devil(i, j); 
+			sQ.offer(new int[] {sx, sy, 0});
+			visited[sx][sy] = true; 
+			int answer = -1; 
+			
+			while(!sQ.isEmpty()) {
+				int[] cur = sQ.poll(); 
+				int x = cur[0], y = cur[1], t = cur[2];
+				
+				if(devTime[x][y] <= t && mat[x][y] != 'S') continue; 
+				
+				for(int d=0; d<4; d++) {
+					int nx = x + dx[d];
+					int ny = y + dy[d]; 
+					int nt = t+1; 
+					if(0<=nx && nx<N && 0<=ny && ny<M && !visited[nx][ny]) {
+						if(mat[nx][ny] == 'D') {
+							answer = nt; 
+							sQ.clear(); 
+							break; 
+						}
+						if(mat[nx][ny] == '.' && devTime[nx][ny] > nt) {
+							visited[nx][ny] = true; 
+							sQ.offer(new int[] {nx, ny, nt});
+						}
 					}
 				}
 				
-				// 그때 S가 갈 수 있는 곳을 찾는다. 
-				int qSize = q.size(); 
-				for(int i=0; i<qSize; i++) {
-					// 큐의 정점을 뽑고 
-					int[] cur = q.poll(); 
-					// 그게 목적지이면 빼고 
-					if(mat[cur[0]][cur[1]] == 'D') {
-						able = true; 
-						break; 
-					}
-					// 아니라면, 주변 4방탐색을 하면서 갈 수 있는 곳을 큐에 모두 넣는다. 
-					for(int d=0; d<4; d++) {
-						int nx = cur[0] + dx[d]; 
-						int ny = cur[1] + dy[d]; 
-						// 조건. 1) 범위 2) *이거나 X이면 못감 3) 방문 안한 곳이어야 함 
-						if(0<=nx && nx<N && 0<=ny && ny<M) {
-							if(v[nx][ny] || mat[nx][ny] == '*' || mat[nx][ny] == 'X') continue; 
-							q.add(new int[] {nx, ny});
-							v[nx][ny] = true; 
-						}
-					} 
-				}
 			}
-			System.out.println(time);
-			q.clear(); 
+			sb.append("#").append(tc).append(" ").append(answer == -1 ? "GAME OVER": answer).append("\n");				
 		}
+		System.out.println(sb.toString());
 	}
 	static int N, M; 
 	static char[][] mat; 
+	static int[][] devTime; 
+	static boolean[][] visited; 
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
-	static ArrayDeque<int[]> q; 
-	static boolean[][] v; 
-	static void devil(int i, int j) {
-		for(int d=0; d<4; d++) {
-			int ni = i+dx[d], nj = j+dy[d]; 
-			if(0<=ni && ni<N && 0<=nj && nj<M) {
-				if(mat[i][j] == 'D' || mat[i][j] == 'X') continue; 
-				mat[ni][nj] = '*';
-			}
-		}
-	}
+
 }
