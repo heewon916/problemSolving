cd59c6dbd27f9483314839b13dbdc026529f38e6
kimhiwon <kimhiwon@naver.com>
2025-08-25 10:11:54 +0900

    add: 0825 swea 1247 solved

--- 0825/src/Solution_d5_1247_최적경로_서울_8반_김희원.java
+++ 0825/src/Solution_d5_1247_최적경로_서울_8반_김희원.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*; 
 
-public class Solution_D5_1247_최적경로_서울_8반_김희원 {
+public class Solution_d5_1247_최적경로_서울_8반_김희원 {
 
 	public static void main(String[] args) throws Exception{
 		// 회사에서 n명의 고객 모두 방문하고 집으로 돌아감 
@@ -35,6 +35,7 @@ public class Solution_D5_1247_최적경로_서울_8반_김희원 {
 		// 시간 복잡도는 각 고객 집마다 N-k씩 => 결국 순열처럼 돌아간다. 
 		// 3,628,800 << 2,000,000,000
 		// 순열로 그냥 가보죠 
+		System.setIn(new FileInputStream("C:\\SSAFY\\homework\\0825\\Input1247.txt"));
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -43,72 +44,52 @@ public class Solution_D5_1247_최적경로_서울_8반_김희원 {
 		
 		for(int t=1; t<=T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			List<int[]> homes = new LinkedList<>(); 
-			boolean[] visited = new boolean[N];
+			answer = Integer.MAX_VALUE; 
+			N = Integer.parseInt(st.nextToken());
+			homes = new ArrayList<>(); 
+			visited = new boolean[N];
 			st = new StringTokenizer(br.readLine(), " ");
 			int sx = Integer.parseInt(st.nextToken());
 			int sy = Integer.parseInt(st.nextToken());
 			int tx = Integer.parseInt(st.nextToken());
 			int ty = Integer.parseInt(st.nextToken());
-			System.out.println("초기화: sx = " + sx + " sy = " + sy + " tx = " + tx + " ty =" + ty );
 			for(int i=0; i<N; i++) {
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				homes.add(new int[] {x, y});
 			}
-//			homes.add(new int[] {tx, ty});
-			for(int[] a : homes) System.out.print(Arrays.toString(a) + " "); System.out.println();
-			// 시작점에 대해서 
-			// 최소 거리의 집을 찾아서  
-			// 그걸 시작점에 대해서 다시 반복하기 
-			// 언제 끝나냐면 sx, sy가 tx, ty가 되면 
-			int answer = -1; 
-			while(true) {
-				if(sx == tx && sy == ty) {
-					System.out.printf("목적지 도착: sx=%d, sy=%d\n", sx, sy);
-					break;
-				}
-				int min_dist = Integer.MAX_VALUE; 
-				int min_x = -1, min_y = -1; 
-				// sx, sy에 대해서 
-				// 방문하지 않았고 맨허튼 거리가 최소인 집을 찾아야 함 
-				// sx, sy에 대해 거리를 계산하고 가장 가까운 지점을 찾으면 됨 
-				// 가장 가까운 지점은 sx, sy가 다시 됨 
-				// homes에 내 집까지 넣었더니 문제 발생함 
-				// 회사에서 내 집이 가장 가까운 경우가 되는 것임 
-				int idx = 0, min_idx = -1; 
-				for(int[] cur: homes) {
-					int cx = cur[0], cy = cur[1];
-					if(cx == tx && cy == ty) continue; 
-					int dist = Math.abs(cx - sx) + Math.abs(cy - sy);
-					if((min_dist > dist) && !visited[idx]) {
-						min_dist = dist; 
-						min_x = cx; min_y = cy; 
-						min_idx = idx; 
-					}
-					idx++; 
-				}
-				if(min_idx == -1) break;
-				sx = min_x; sy = min_y; 
-				visited[min_idx] = true; 	
-				answer += min_dist; 
-				System.out.printf("sx=%d, sy=%d -> min_dist=%d/ answer=%d ", sx, sy, min_dist, answer);
-				System.out.println(Arrays.toString(visited));
-			}
-			
-			System.out.println(answer + Math.abs(sx-tx) + Math.abs(sy-ty));
-//0 0 
-//100 100 
-//70 40 
-//30 10 
-//10 5
-//90 70 
-//50 20 
-//
-//0 0 - 10 5 -> 15
-//10 5 - 30 10 -> 20 + 5 = 25 
-//30 10 - 
+			b = new int[N+2][2]; 
+			b[0] = new int[]{sx, sy}; 
+			b[N+1] = new int[]{tx, ty}; 
+			perm(1, 0); 
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	static int N; 
+	static int[][] b; 
+	static boolean[] visited; 
+	static List<int[]> homes;
+	static int answer = Integer.MAX_VALUE;
+	static void perm(int cnt, int sum) {
+		if(sum >= answer) return; 
+		if(cnt == N+1) {
+//			int min_dist = 0; 
+//			for(int i=0; i<N+1; i++) {
+//				int dist = Math.abs(b[i][0] - b[i+1][0]) + Math.abs(b[i][1] - b[i+1][1]);
+//				min_dist += dist; 
+//			}
+//			answer = Math.min(answer, min_dist);
+			sum += Math.abs(b[N][0] - b[N+1][0]) + Math.abs(b[N][1] - b[N+1][1]);
+			if(sum < answer) answer = sum; 
+			return;  
+		}
+		for(int i=0; i<N; i++) {
+			if(visited[i]) continue; 
+			b[cnt] = homes.get(i);
+			visited[i] = true; 
+			perm(cnt+1, sum + Math.abs(b[cnt-1][0] - b[cnt][0]) + Math.abs(b[cnt-1][1] - b[cnt][1]));
+			visited[i] = false; 
 		}
 	}
 }
