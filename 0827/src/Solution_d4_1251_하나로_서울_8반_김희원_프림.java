c83dc6c931b32a4f07d7e51ef9ccd7b49ebdf9eb
kimhiwon <kimhiwon@naver.com>
2025-08-28 09:55:17 +0900

    add: 0827 swea 3124, 1251 to prim

--- 0827/src/Solution_d4_1251_하나로_서울_8반_김희원_프림.java
+++ 0827/src/Solution_d4_1251_하나로_서울_8반_김희원_프림.java
@@ -1,9 +1,125 @@
+import java.io.*;
+import java.util.*;
 
 public class Solution_d4_1251_하나로_서울_8반_김희원_프림 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		StringBuilder sb = new StringBuilder(); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for(int t=1; t<=T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			
+			long[] xs = new long[N]; 
+			long[] ys = new long[N]; 
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int i=0; i<N; i++) {
+				xs[i] = Long.parseLong(st.nextToken());
+			}
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int i=0; i<N; i++) {
+				ys[i] = Long.parseLong(st.nextToken());
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			double E = Double.parseDouble(st.nextToken()); 
+			
+			long[][] dist = new long[N][N]; 
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					long dx = xs[i] - xs[j]; 
+					long dy = ys[i] - ys[j]; 
+					dist[i][j] = dx*dx + dy*dy; 
+				}
+			}
+			
+			boolean[] v = new boolean[N]; 
+//			int[] prim = new int[N]; 
+//			Arrays.fill(prim, Integer.MAX_VALUE);
+			long[] prim = new long[N]; 
+			Arrays.fill(prim, Long.MAX_VALUE);
+			prim[0] = 0;
+			
+			long mst = 0; 
+			int cnt = 0; 
+			for(int i=0; i<N; i++) {
+				int minVertex = -1; 
+				long min = Long.MAX_VALUE; 
+				
+				for(int j=0; j<N; j++) {
+					if(!v[j] && min > prim[j]) {
+						min = prim[j]; 
+						minVertex = j; 
+					}
+				}
+				
+				v[minVertex] = true; 
+				mst += min; 
+				// System.out.println("minVertex = " + minVertex + " mst = " + mst);
+				if(++cnt == N) break;
+				
+				for(int j=0; j<N; j++) {
+					if(!v[j] && prim[j] > dist[minVertex][j]) {
+						prim[j] = dist[minVertex][j]; 
+					}
+				}
+			}
+			// System.out.println();
+			
+			long answer = Math.round(mst * E); 
+			sb.append("#").append(t).append(" ").append(answer).append("\n"); 
+		}
+		System.out.println(sb.toString());
+		
 	}
 
 }
+/*
+[이해] 
+N개의 섬 연결하기 
+환경 부담금 = 환경부담세율 E * (각 해저 터널 길이의 제곱)
+총 환경 부담금 최소로 지불하고, N개의 섬을 모두 연결 
+
+[입력] 
+N 섬의 개수 ~10^3
+x좌표 ~10^6
+y좌표 ~10^6 
+부담세율 E 
+
+[해결] 
+부담금이 최소가 되려면 각 해저 터널을 서로 연결하는 최소신장트리를 구해야 함 
+입력은 정점 중심으로 들어오고 있음 
+프림 ? 
+
+--- 
+int[] prim <- 트리에서 각 해저 터널로 가는 최소 비용을 담는다. 
+시작정점은 아무거나 골라도 될 듯
+각 정점 좌표 int[] xs = new int[N]; ys도 동일함 
+각 좌표 간의 거리 미리 구하기 int[][] dist -> (xs[i] - xs[j])*(xs[i] - xs[j]) + (ys[i] - ys[j])*(ys[i] - ys[j])
+트리에 포함됐는지 확인하기 boolean[] v = new int[N];  
+프림: 
+프림 배열의 인덱스 i  <-> xs[i], ys[i] 좌표 
+프림 배열은 Integer.MAX_VALUE로 초기화하기 
+prim[0] = 0; 
+int mst = 0; 트리 비용 값 누적 
+int cnt = 0; 트리에 포함된 정점 개수 
+for(int i=0; i<N; i++) 
+	1. 최소 정점과 최소거리 찾기 
+	int minVertex = -1; 
+	int min = Integer.MAX_VALUE; 
+	for(int i=0; i<N; i++) 
+		if(!v[i] && min > prim[i]) 
+				min = prim[i]; 
+				minVertex = i; 
+		
+		v[minVertex] = true; 
+		mst += min; 
+		if(++cnt == N-1) break;
+		
+		for(int i=0; i<N; i++)
+			if(!v[i] && prim[i] > dist[minVertex][i]) 
+				prim[i] = dist[minVertex][i]; 
+*/
--- 0827/src/Solution_d4_3124_최소스패닝트리_서울_8반_김희원_프림.java
+++ 0827/src/Solution_d4_3124_최소스패닝트리_서울_8반_김희원_프림.java
@@ -1,9 +1,109 @@
+import java.io.*; 
+import java.util.*;
 
 public class Solution_d4_3124_최소스패닝트리_서울_8반_김희원_프림 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		StringBuilder sb = new StringBuilder(); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int t=1; t<=T; t++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			
+			List<long[]>[] g = new List[V+1]; for(int i=0; i<V+1; i++) g[i] = new ArrayList<>(); 
+			for(int i=0;i<E; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				long w = Integer.parseInt(st.nextToken());
+				g[a].add(new long[] {b, w});
+				g[b].add(new long[] {a, w});
+				
+			}
+			long[] prim = new long[V+1]; 
+			boolean[] v = new boolean[V+1]; 
+			PriorityQueue<long[]> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[1], o2[1])); 
+			Arrays.fill(prim, Long.MAX_VALUE);
+			prim[1] = 0L; 
+			pq.add(new long[] {1, prim[1]});
+			long mst = 0L; 
+			int cnt = 0; 
+			while(!pq.isEmpty()) {
+				long[] cur = pq.poll();
+				int minVertex = (int)cur[0]; 
+				long min = cur[1]; 
+				if(v[minVertex]) continue; 
+				v[minVertex] = true; 
+				mst += min; 
+				
+				if(++cnt == V) break; 
+				
+				for(long[] adj: g[minVertex]) {
+					int next = (int)adj[0]; long nextW = adj[1]; 
+					if(!v[next] && prim[next] > nextW) {
+						prim[next] = nextW; 
+						pq.add(new long[] {next, prim[next]});
+					}
+				}
+			}
+			sb.append("#").append(t).append(' ').append(mst).append("\n"); 
+		}
+		System.out.println(sb.toString());
 
 	}
 
 }
+/*
+[이해]
+최소 스패닝 트리 구하기 
+주어진 그래프의 모든 정점 연결하는 부분 그래프 중 그 가중치의 합이 최소인 트리 
+
+정점 중심은 프림 
+간선 중심은 크루스칼 
+
+정점 개수 v ~10^5
+간선 개수 E ~2 * 10^5
+가중치 값 w ~10^6 
+
+[입력] 
+간선 리스트 입력 
+a, b, w 
+
+[설계]
+프림 + PQ O(E logV)
+- 간선이 주어져서 크루스칼로도 해결 가능하지만 
+- 간선 정보를 정점 정보로 바꿔서 해결해보자 
+
+int[] prim <- 트리 전체와의 거리 중 최소 비용 
+boolean[] v <- 트리에 포함된 정점 
+List<int[]>[] g <- 간선 리스트 
+
+PriorityQueue<int[]> pq + (o1, o2)->Integer.compare(o1[1], o2[1]) <- 가중치 기준으로 오름차순 정렬됨 
+<- (정점번호, 트리와의 최소거리) 넣는다. 
+
+prim[0] = 0; 
+prim 나머지는 MAX값으로 초기화 
+
+pq에 0과 prim[0] 값 전달 
+pq에 빌때까지 
+	cur <- 정점, prim[정점] 
+	
+	cur이 최소비용 정점이니까 초기화하고 
+	int minVertex = cur[0]
+	int min = cur[1]
+	
+	방문했었던 애면 패스하고 아니면 mst에 포함시켜야 됨 
+	if(v[minVertex]) continue; 
+	v[minVertex] = true; 
+	mst += min 
+	
+	만약 정점 모두 트리에 포함했으면 끝내면 됨 
+	if(++cnt == N-1) break; 
+	
+	minVertex에 인접한 정점에 대해서, minVertex를 통해서 갈 떄 최소 비용이 발생하면 prim[adj] > g[minVertex][adj]이면 prim[adj]갱신해줘야됨 
+*/
