68f7633ae9915dfb1b9e3d41435000d2376612c2
kimhiwon <kimhiwon@naver.com>
2025-09-18 23:19:54 +0900

    PS: swea 2477 차량정비소

---
+++ work_algo/src/Main_bj_15683_.java
@@ -0,0 +1,10 @@
+import java.io.*; 
+import java.util.*; 
+public class Main_bj_15683_감시  {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+
+    }
+}
