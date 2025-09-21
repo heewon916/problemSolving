9300ae5d5994906e3b6846668abd1c57e1652985
kimhiwon <kimhiwon@naver.com>
2025-09-21 15:58:58 +0900

    PS: bj 3154 done; bj 31860 ing;

---
+++ work_algo/bj/Main_bj_31860_열심히일하는중.java
@@ -0,0 +1,11 @@
+import java.io.*; 
+import java.util.*; 
+
+public class Main_bj_31860_열심히일하는중 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder(); 
+        
+    }
+}
