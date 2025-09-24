56310b88179d21dc5d35d03d72ca43e6823cd70b
kimhiwon <kimhiwon@naver.com>
2025-09-25 00:48:11 +0900

    .

---
+++ work_algo/bj/Main_bj_1327_소트게임.java
@@ -0,0 +1,25 @@
+import java.io.*; 
+import java.util.*; 
+public class Main_bj_1327_소트게임 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null; 
+
+    }
+}
+/*
+1~N까지의 숫자
+N자리의 순열 
+K 
+
+어떤 수 뒤집으면 
+그 수 포함해서, 오른쪽으로 K개의 수 뒤집기 
+i, .. i+(K-1)
+
+반드시 K개의 수를 뒤집어야 한다
+숫자를 뒤집어도, 내 뒤에 K-1개의 수가 있어야 한다 
+
+순열 오름차순으로 만드려고 함. 최소 몇 개 선택해야 됨? 
+
+
+ */
